284c04b153290e07a9b5c3a2710d4bf8976b573b
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2022-03-22 21:46:55 -0400

    started implementation of DM conversations

--- src/main/java/DMController.java
+++ src/main/java/DMController.java
@@ -3,25 +3,68 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class DMController extends ListenerAdapter {
 
-    private User user;
-    private PrivateChannel channel;
+    private final User user;
+    private final PrivateChannel channel;
+    private String sent = "";
+    private int turns = 0;
 
     public DMController(PrivateChannel channel, String startMessage) {
         this.user = channel.getUser();
         this.channel = channel;
-        SendMessage(startMessage);
+        sendMessage(startMessage);
     }
 
-    public boolean SendMessage(String message){
+    public boolean userMatch(User user) {
+        return this.user.equals(user);
+    }
+
+    public void setMessage(String message) {
+        sent = message;
+    }
+
+    public int random(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public boolean sentHas(String query) {
+        return sent.toLowerCase().contains("flag");
+    }
+
+    public void sendMessage(String message){
+        channel.sendTyping().queue();
         try {
+            Thread.sleep(random(1000, 10000));
             channel.sendMessage(message).queue();
-        }catch(ErrorResponseException ignored) {
-            System.out.println("error");
+        } catch (ErrorResponseException ignored) {
+            System.out.println("ERROR: Could not send message to " + user.getAsTag());
+            ignored.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        return true;
     }
 
+    public void conversation() throws InterruptedException {
+        if (sentHas("flag")) {
+            int send = random(1, 3);
+            switch (send) {
+                case 1:
+                    sendMessage("lets not talk about that right now");
+                    break;
+                case 2:
+                    sendMessage("thats not important");
+                    break;
+                case 3:
+                    sendMessage("not now, ok?");
+                    break;
+                default:
+                    sendMessage("Default response; flag");
+            }
+        } else {
+            sendMessage("Default response.");
+        }
+    }
 }
--- src/main/java/MessageListener.java
+++ src/main/java/MessageListener.java
@@ -1,26 +1,46 @@
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class MessageListener extends ListenerAdapter {
 
+    private LinkedList<User> userList = new LinkedList<User>();
+    private LinkedList<DMController> dmControllerList = new LinkedList<DMController>();
+
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
+        User sender = msg.getAuthor();
         if (msg.getContentRaw().equals("!ping")) {
             MessageChannel channel = event.getChannel();
             long time = System.currentTimeMillis();
             channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response /* => Message */ -> {
-                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                    });
+                    .queue(response /* => Message */ -> response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue());
         }
-        if (!msg.isFromGuild() && !Objects.equals(event.getAuthor(), event.getJDA().getSelfUser())) {
-            msg.getAuthor().openPrivateChannel()
-                    .flatMap(channel -> channel.sendMessage("DM received!"))
-                    .queue();
+        if (!msg.isFromGuild() && !Objects.equals(sender, event.getJDA().getSelfUser())) {
+            if (!userList.contains(sender)) {
+                userList.add(sender);
+                dmControllerList.add(new DMController(msg.getPrivateChannel(), "whats up?"));
+            } else {
+                for (DMController dm : dmControllerList) {
+                    if (dm.userMatch(sender)) {
+                        dm.setMessage(msg.getContentRaw());
+                        try {
+                            dm.conversation();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    }
+                }
+            }
+//            msg.getAuthor().openPrivateChannel()
+//                    .flatMap(channel -> channel.sendMessage("DM received!"))
+//                    .queue();
         }
     }
 }
