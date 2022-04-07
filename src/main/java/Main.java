d78f659a0c5700ec429e6972b64ae7e52be599ce
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2022-04-07 11:41:18 -0400

    added in-code switch for gui or not

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
 import javax.security.auth.login.LoginException;
+import java.io.PrintStream;
 
 public class Main {
 
     public static void main(String[] args) throws LoginException {
 
-        WindowActions wActs = new WindowActions();
+        // Set to true if the GUI is desired
+        boolean gui = false;
 
-        MainForm mainForm = new MainForm();
-        wActs.displayWindow(mainForm);
+        if (gui) {
+            WindowActions wActs = new WindowActions();
+
+            MainForm mainForm = new MainForm();
+            wActs.displayWindow(mainForm);
+        } else {
+            Bot bot = new Bot();
+        }
     }
 }
